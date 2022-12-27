9b6430e1ede1e94eea265f6f6b87788c6c63ff19
Carli Steele <121509441+SnowFur2030@users.noreply.github.com>
2022-12-27 13:37:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,4 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
